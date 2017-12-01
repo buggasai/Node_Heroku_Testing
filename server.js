2f6f1fd07b3471372feab21f038148671a220b4a
sai baba <buggasai@gmail.com>
2017-12-01 16:03:30 +0530

    dssa

--- server.js
+++ server.js
@@ -17,8 +17,7 @@ bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-//app.use(cors());
+app.use(morgan('dev'));
 
 app.use(function (req, res, next) {
 res.header("Access-Control-Allow-Origin", "*");
