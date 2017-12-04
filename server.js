7895e933c4a83fd30f99afbd87deeed23a1aff52
sai baba <buggasai@gmail.com>
2017-12-04 12:22:53 +0530

    api added

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 var express = require('express'),
-app = express(),
-port = process.env.PORT || 3000,
-mongoose = require('mongoose'),
-morgan = require('morgan'),
-jwt = require('jsonwebtoken'),
-multer = require('multer'),
+    app = express(),
+    port = process.env.PORT || 3000,
+    mongoose = require('mongoose'),
+    morgan = require('morgan'),
+    jwt = require('jsonwebtoken'),
+    multer = require('multer'),
 
-bodyParser = require('body-parser');
 
- 
+    User = require('./api/models/userModel'),
+    Owner = require('./api/models/ownerModel'),
+    Building = require('./api/models/buildingModel'),
+    Tenant = require('./api/models/tenantModel'),
+    Vendor = require('./api/models/vendorModel'),
+    Expense = require('./api/models/expenseModel'),
+    Payment = require('./api/models/paymentModel'),
+
+    bodyParser = require('body-parser');
+
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://192.168.100.23/RMSDB');
 mongoose.connect('mongodb://heroku_r9hn0jzn:echn9ckdip4644i79p2j4blun8@ds129796.mlab.com:29796/heroku_r9hn0jzn');
-
-
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -29,9 +34,11 @@ app.use(function (req, res, next) {
 
 // Set up a URL route
 app.get("/", function (req, res) {
-res.send("Heroku Demo!");
+    res.send("Heroku Demo!");
 });
 
+var routes = require('./api/routes/routes');
+routes(app);
 
 app.listen(port);
 
